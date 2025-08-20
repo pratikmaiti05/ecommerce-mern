88e5b667d14536abf3593188fca5713c7a6c057f
pratikmaiti05 <maitipratik83@gmail.com>
2025-08-20 13:00:42 +0530

    baseurl change

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance=axios.create({
-  baseURL:"https://ecommerce-mern-qqh9.onrender.com",
+  baseURL:"http://localhost:3000", // Update this to your backend URL
   withCredentials:true
 })
 export default instance
